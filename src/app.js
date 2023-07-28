364515c5fd998bd46ced040b96136a702e8360ea
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-27 19:29:40 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import handlebars from 'express-handlebars'
+import cookieParser from "cookie-parser";
+import jwtRouter from './routers/jwt.router.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser('secret'))
+app.engine('handlebars', handlebars.engine())
+app.set('views', './src/views')
+app.set('view engine', 'handlebars')
+
+app.use('/jwt', jwtRouter)
+app.get('/login', (req, res) => {
+    res.render('index')
+})
+
+app.listen(8080, () => console.log('Server Up!'))
